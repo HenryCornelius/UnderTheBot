ceb9f4f6c6eeabc2af558f4f5943ceb7c80c667e
Tobia DENIS <tobia0822@gmail.com>
2022-08-26 01:33:08 +0200

    Ajout role champions

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 
 import discord
 import random
+import lolChamp
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -50,13 +51,13 @@ class MyClient(discord.Client):
                 for i in range(2):
                     if chiffres[i].isdigit() != True:
                         await message.reply("L'argument n°"+str(i+1)+" n'est pas un chiffre", mention_author=True)
-                await message.reply("1er dé: "+str(random.randrange(1,int(chiffres[0]),1))+", 2eme dé: "+str(random.randrange(1,int(chiffres[1]),1)), mention_author=True)
+                await message.reply(":8ball: "+str(random.randrange(1,int(chiffres[0]),1))+", :8ball: "+str(random.randrange(1,int(chiffres[1]),1)), mention_author=True)
             else:
-                await message.reply("1er dé: "+str(random.randrange(1,int(chiffre),1))+", 2eme dé: "+str(random.randrange(1,int(chiffre),1)), mention_author=True)
+                await message.reply(":8ball: "+str(random.randrange(1,int(chiffre),1))+", :8ball: "+str(random.randrange(1,int(chiffre),1)), mention_author=True)
 
         if message.content.startswith('!randomChamp'):
-            listeChamp = ["Aatrox", "Ahri", "Akali", "Akshan", "Alistar", "Amumu", "Anivia", "Annie", "Aphelios", "Ashe", "Aurelion Sol", "Azir", "Bard", "Bel'Veth", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Gwen","Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern", "Janna", "Jarvan IV", "Jax", "Jayce", "Jhin", "Jinx", "Kai'Sa", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lillia", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Neeko", "Nidalee", "Nilah", "Nocturne", "Nunu and Willump", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Pyke", "Qiyana", "Quinn", "Rakan", "Rammus", "Rek'Sai", "Rell", "Renata Glasc","Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Samira", "Sejuani", "Senna", "Seraphine", "Sett", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Sylas", "Syndra", "Tahm Kench", "Taliyah", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vex", "Vi", "Viego", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xayah", "Xerath", "Xin Zhao", "Yasuo", "Yone", "Yorick", "Yuumi", "Zac", "Zed", "Zeri", "Ziggs", "Zilean", "Zoe", "Zyra"]
-            await message.reply("Voici... Votre champion : " + listeChamp[random.randrange(0,len(listeChamp),1)], mention_author=True)
+            await message.reply("Voici... Votre champion : " + lolChamp.listeChampion[random.randrange(0,len(lolChamp.listeChampion),1)], mention_author=True)
+        
 
 intents = discord.Intents.default()
 intents.message_content = True
