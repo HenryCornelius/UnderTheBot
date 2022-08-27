508f531a1799e600377aadf372489bbd28756782
Tobia DENIS <tobia0822@gmail.com>
2022-08-27 02:20:33 +0200

    Ajout checkMate

---
+++ compte.py
@@ -0,0 +1,10 @@
+class Compte():
+    def __init__(self,discordName, lolName):
+        self.lolname = lolName
+        self.discordname = discordName
+
+Tobia     = Compte('Tobia', 'Azko')
+Biboun    = Compte('Bìboun', 'Biboun')
+Milan     = Compte('Milań', 'Milanetbruce')
+
+listeMembre = [Tobia, Biboun, Milan]
