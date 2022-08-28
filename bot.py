2152a4887477cadfeef56c5bfd0acfebe3a640ff
Tobia DENIS <tobia0822@gmail.com>
2022-08-28 17:37:32 +0200

    Externalisation et modif embeds

--- bot.py
+++ bot.py
@@ -6,73 +6,12 @@ from riotwatcher import LolWatcher, ApiError
 import random
 import lolChamp
 import compte
+import embed
 import nacl
 
 api_key = 'RGAPI-84d70ce7-80c8-44b7-b822-209762bc03ef'
 watcher = LolWatcher(api_key)
 my_region = 'euw1'
-
-def create_help_embed():
-    embed = discord.Embed(title='**COMMANDS**',
-                        description='', colour=discord.Colour.green())
-    embed.add_field(name='`!1d<chiffre>`',
-                    value='Lance un dé. Exemple: `!1d6`',
-                    inline=False)
-    embed.add_field(name='`!2d<chiffre> [<chiffre>]`',
-                    value='Lance deux dés. Exemples: `!2d6` - `!2d6 8`',
-                    inline=False)
-    embed.add_field(name='`!randomChamp`',
-                    value='Selectionne un champion de manière aléatoire sur son rôle principal.',
-                    inline=False)
-    embed.add_field(name='`!checkRank <summoner name> [<type ranked>]`',
-                    value='Vérifie le rang du pseudo entré. Exemples: `!checkRank Tobia` - `!checkRank Tobia TFT`',
-                    inline=False)
-    return embed
-
-def create_mates_embed1(mates, bluewin, blueObj):
-    embed1 = discord.Embed(title=bluewin,
-                        description='', colour=discord.Colour.blue())
-    embed1.add_field(name=mates[0][0],
-                    value=mates[0][1],
-                    inline=False)
-    embed1.add_field(name=mates[1][0],
-                    value=mates[1][1],
-                    inline=False)
-    embed1.add_field(name=mates[2][0],
-                    value=mates[2][1],
-                    inline=False)
-    embed1.add_field(name=mates[3][0],
-                    value=mates[3][1],
-                    inline=False)
-    embed1.add_field(name=mates[4][0],
-                    value=mates[4][1],
-                    inline=False)
-    embed1.add_field(name="Objectifs",
-                    value=blueObj[0]+" Drake / "+blueObj[1]+" Nash / "+blueObj[2]+" Herald",
-                    inline=False)
-    return embed1
-def create_mates_embed2(mates, redwin, redObj):
-    embed2 = discord.Embed(title=redwin,
-                        description='', colour=discord.Colour.red())
-    embed2.add_field(name=mates[5][0],
-                    value=mates[5][1],
-                    inline=False)
-    embed2.add_field(name=mates[6][0],
-                    value=mates[6][1],
-                    inline=False)
-    embed2.add_field(name=[mates[7][0]]("https://euw.op.gg/summoners/euw/"+mates[7][0]),
-                    value=mates[7][1],
-                    inline=False)
-    embed2.add_field(name=mates[8][0],
-                    value=mates[8][1],
-                    inline=False)
-    embed2.add_field(name=mates[9][0],
-                    value=mates[9][1],
-                    inline=False)
-    embed2.add_field(name="Objectifs",
-                    value=redObj[0]+" Drake / "+redObj[1]+" Nash / "+redObj[2]+" Herald",
-                    inline=True)
-    return embed2
 
 class MyClient(discord.Client):
 
@@ -95,7 +34,7 @@ class MyClient(discord.Client):
         #                                                                                                                  #
         ####################################################################################################################
         if message.content.startswith('!help'):
-            await message.channel.send(content=None, embed=create_help_embed())
+            await message.channel.send(content=None, embed=embed.create_help_embed())
             return
 
 
@@ -247,13 +186,14 @@ class MyClient(discord.Client):
             mates = []
             for j in match_detail['info']['participants']:
                 mate = watcher.summoner.by_id(my_region, j["summonerId"])
-                mates_name = str(mate['name']) + " - " + str(j['championName']) + " - " + str(j['kills']) + "/" + str(j['deaths']) + "/" + str(j['assists']) + " - " + str(j['neutralMinionsKilled']) + "cs"
+                mates_name = str(mate['name']) + " - " + str(j['role']) 
+                mates_champ = str(j['championName']) + " - " + str(j['kills']) + "/" + str(j['deaths']) + "/" + str(j['assists']) + " - " + str(j['totalMinionsKilled']) + "cs"
                 mates_rank = watcher.league.by_summoner(my_region, mate['id'])
                 for k in range(len(mates_rank)):
                     if mates_rank[k]['queueType'] == "RANKED_SOLO_5x5":
                         rank_solo = mates_rank[k]
                         mates_solo_rank = rank_solo['tier'] + " " + rank_solo['rank'] + " - " + str(rank_solo['leaguePoints']) + " LP "
-                mates.append([mates_name, mates_solo_rank])
+                mates.append([mates_name, mates_solo_rank, mates_champ])
             
             if match_detail['info']['teams'][0]['win']:
                 bluewin = "**WINNERS**  :green_circle:"
@@ -271,9 +211,9 @@ class MyClient(discord.Client):
             redHeralds = str(match_detail['info']['teams'][1]['objectives']['riftHerald']['kills'])
             redObj = [redDragons, redBarons, redHeralds]
             
-            await message.channel.send(content=None, embed=create_mates_embed1(mates, bluewin, blueObj))
+            await message.channel.send(content=None, embed=embed.create_mates_embed1(mates, bluewin, blueObj))
             
-            await message.channel.send(content=None, embed=create_mates_embed2(mates, redwin, redObj))
+            await message.channel.send(content=None, embed=embed.create_mates_embed2(mates, redwin, redObj))
             return
             
 
---
+++ embed.py
@@ -0,0 +1,87 @@
+def create_help_embed():
+    embed = discord.Embed(title='**COMMANDS**',
+                        description='', colour=discord.Colour.green())
+    embed.add_field(name='`!1d<chiffre>`',
+                    value='Lance un dé. Exemple: `!1d6`',
+                    inline=False)
+    embed.add_field(name='`!2d<chiffre> [<chiffre>]`',
+                    value='Lance deux dés. Exemples: `!2d6` - `!2d6 8`',
+                    inline=False)
+    embed.add_field(name='`!randomChamp`',
+                    value='Selectionne un champion de manière aléatoire sur son rôle principal.',
+                    inline=False)
+    embed.add_field(name='`!checkRank <summoner name> [<type ranked>]`',
+                    value='Vérifie le rang du pseudo entré. Exemples: `!checkRank Tobia` - `!checkRank Tobia TFT`',
+                    inline=False)
+    return embed
+
+def create_mates_embed1(mates, bluewin, blueObj):
+    embed1 = discord.Embed(title=bluewin,
+                        description='', colour=discord.Colour.blue())
+    embed1.add_field(name=mates[0][0],
+                    value=mates[0][1],
+                    inline=True)
+    embed1.add_field(name=mates[0][2],
+                    value="",
+                    inline=True)
+    embed1.add_field(name=mates[1][0],
+                    value=mates[1][1],
+                    inline=True)
+    embed1.add_field(name=mates[1][2],
+                    value="",
+                    inline=True)
+    embed1.add_field(name=mates[2][0],
+                    value=mates[2][1],
+                    inline=True)
+    embed1.add_field(name=mates[2][2],
+                    value="",
+                    inline=True)
+    embed1.add_field(name=mates[3][0],
+                    value=mates[3][1],
+                    inline=True)
+    embed1.add_field(name=mates[3][2],
+                    value="",
+                    inline=True)
+    embed1.add_field(name=mates[4][0],
+                    value=mates[4][1],
+                    inline=True)
+    embed1.add_field(name=mates[4][2],
+                    value="",
+                    inline=True)
+    embed1.set_footer(text=blueObj[0]+" Drake / "+blueObj[1]+" Nash / "+blueObj[2]+" Herald")
+    return embed1
+def create_mates_embed2(mates, redwin, redObj):
+    embed2 = discord.Embed(title=redwin,
+                        description='', colour=discord.Colour.red())
+    embed2.add_field(name=mates[5][0],
+                    value=mates[5][1],
+                    inline=True)
+    embed2.add_field(name=mates[5][2],
+                    value="",
+                    inline=True)
+    embed2.add_field(name=mates[6][0],
+                    value=mates[6][1],
+                    inline=True)
+    embed2.add_field(name=mates[6][2],
+                    value="",
+                    inline=True)
+    embed2.add_field(name=mates[7][0],
+                    value=mates[7][1],
+                    inline=True)
+    embed2.add_field(name=mates[7][2],
+                    value="",
+                    inline=True)
+    embed2.add_field(name=mates[8][0],
+                    value=mates[8][1],
+                    inline=True)
+    embed2.add_field(name=mates[8][2],
+                    value="",
+                    inline=True)
+    embed2.add_field(name=mates[9][0],
+                    value=mates[9][1],
+                    inline=True)
+    embed2.add_field(name=mates[9][2],
+                    value="",
+                    inline=True)
+    embed2.set_footer(text=redObj[0]+" Drake / "+redObj[1]+" Nash / "+redObj[2]+" Herald")
+    return embed2
