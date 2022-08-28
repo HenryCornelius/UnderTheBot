70499a80dff1481c7eef64f283cd064a8f31b16d
Tobia DENIS <tobia0822@gmail.com>
2022-08-29 00:58:07 +0200

    Corrections

--- bot.py
+++ bot.py
@@ -208,9 +208,9 @@ class MyClient(discord.Client):
                     if mates_rank[k]['queueType'] == "RANKED_SOLO_5x5":
                         rank_solo = mates_rank[k]
                         mates_solo_rank = rank_solo['tier'] + " " + rank_solo['rank'] + " - " + str(rank_solo['leaguePoints']) + " LP "
-                mates_damage = str(j['totalDamageDealtToChampions']) + " dmg"
                 mates_vision = str(j['visionScore']) + " vision"
-                mates_gold = str(j['goldEarned']) + " golds - " + mates_vision
+                mates_damage = str(j['totalDamageDealtToChampions']) + " dmg - " + mates_vision
+                mates_gold = str(j['goldEarned']) + " golds"
                 mates = Mates(mates_name,mates_champ,mates_mastery,mates_solo_rank,mates_damage,mates_gold)
                 matesArray.append(mates)
 
