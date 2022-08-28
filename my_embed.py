f1f9369a9c407212631d1b8439b45c556be13e4e
Tobia DENIS <tobia0822@gmail.com>
2022-08-28 17:55:22 +0200

    Test embed

--- my_embed.py
+++ my_embed.py
@@ -26,30 +26,35 @@ def create_mates_embed1(mates, bluewin, blueObj):
     embed1.add_field(name=mates[0][2],
                     value="test",
                     inline=True)
+    embed1.addField('\u200b', '\u200b')
     embed1.add_field(name=mates[1][0],
                     value=mates[1][1],
                     inline=True)
     embed1.add_field(name=mates[1][2],
                     value="test",
                     inline=True)
+    embed1.addField('\u200b', '\u200b')
     embed1.add_field(name=mates[2][0],
                     value=mates[2][1],
                     inline=True)
     embed1.add_field(name=mates[2][2],
                     value="test",
                     inline=True)
+    embed1.addField('\u200b', '\u200b')
     embed1.add_field(name=mates[3][0],
                     value=mates[3][1],
                     inline=True)
     embed1.add_field(name=mates[3][2],
                     value="test",
                     inline=True)
+    embed1.addField('\u200b', '\u200b')
     embed1.add_field(name=mates[4][0],
                     value=mates[4][1],
                     inline=True)
     embed1.add_field(name=mates[4][2],
                     value="test",
                     inline=True)
+    embed1.addField('\u200b', '\u200b')
     embed1.set_footer(text=blueObj[0]+" Drake / "+blueObj[1]+" Nash / "+blueObj[2]+" Herald")
     return embed1
 def create_mates_embed2(mates, redwin, redObj):
@@ -61,29 +66,34 @@ def create_mates_embed2(mates, redwin, redObj):
     embed2.add_field(name=mates[5][2],
                     value="test",
                     inline=True)
+    embed2.addField('\u200b', '\u200b')
     embed2.add_field(name=mates[6][0],
                     value=mates[6][1],
-                    inline=False)
+                    inline=True)
     embed2.add_field(name=mates[6][2],
                     value="test",
                     inline=True)
+    embed2.addField('\u200b', '\u200b')
     embed2.add_field(name=mates[7][0],
                     value=mates[7][1],
                     inline=True)
     embed2.add_field(name=mates[7][2],
                     value="test",
                     inline=True)
+    embed2.addField('\u200b', '\u200b')
     embed2.add_field(name=mates[8][0],
                     value=mates[8][1],
                     inline=True)
     embed2.add_field(name=mates[8][2],
                     value="test",
                     inline=True)
+    embed2.addField('\u200b', '\u200b')
     embed2.add_field(name=mates[9][0],
                     value=mates[9][1],
                     inline=True)
     embed2.add_field(name=mates[9][2],
                     value="test",
                     inline=True)
+    embed2.addField('\u200b', '\u200b')
     embed2.set_footer(text=redObj[0]+" Drake / "+redObj[1]+" Nash / "+redObj[2]+" Herald")
     return embed2
