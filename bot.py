3ff801821b857fd4315b1df0f9a56b1467fb843e
Tobia DENIS <tobia0822@gmail.com>
2022-08-28 18:35:28 +0200

    Corrections date

--- bot.py
+++ bot.py
@@ -189,12 +189,13 @@ class MyClient(discord.Client):
             for j in match_detail['info']['participants']:
                 mate = watcher.summoner.by_id(my_region, j["summonerId"])
                 mates_champ_mastery = watcher.champion_mastery.by_summoner_by_champion(my_region, j["summonerId"], j['championId'])
+                mate_last_game = datetime.datetime.fromtimestamp(mates_champ_mastery['lastPlayTime'] / 1000)
                 mates_role = j['lane']
                 if j['lane'] == "BOTTOM": 
                     mates_role = j['role']
                 mates_name = str(mate['name']) + " - " + str(mates_role) 
                 mates_champ = str(j['championName']) + " - " + str(j['kills']) + "/" + str(j['deaths']) + "/" + str(j['assists']) + " - " + str(j['totalMinionsKilled']) + "cs"
-                mates_mastery = "Derniere partie de " + str(j['championName']) + " le " + str(datetime.datetime.fromtimestamp(mates_champ_mastery['lastPlayTime'] / 1000)) 
+                mates_mastery = "Derniere partie de " + str(j['championName']) + " le " + str(mate_last_game.day) + "-" + str(mate_last_game.month) + "-" + str(mate_last_game.year) + " " + + str(mate_last_game.hour) + ":" + str(mate_last_game.minute)
                 mates_rank = watcher.league.by_summoner(my_region, mate['id'])
                 for k in range(len(mates_rank)):
                     if mates_rank[k]['queueType'] == "RANKED_SOLO_5x5":
